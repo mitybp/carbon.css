1af937b2f182696b2dc511ccb1d4cc490a03316f
mitybp <dimitribpusch21@gmail.com>
2023-11-07 16:16:03 -0300

    [docs]: add padding-margin
    [carbon.css]: add carbon.min.css

--- script.js
+++
@@ -1,8 +0,0 @@
-copyLinkImport = () => {
-  document.getElementById("copyLinkImport").innerHTML =
-    "<i class='ph ph-check'></i>";
-  setTimeout(() => {
-    document.getElementById("copyLinkImport").innerHTML =
-      "<i class='ph ph-clipboard'><i/>";
-  }, 2000);
-};
